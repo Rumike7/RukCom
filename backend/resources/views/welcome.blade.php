9a8524bde3d37aa8db8c30c0c0a8c220ae0ed685
Rumike7 <martintossou2@gmail.com>
2023-07-04 17:56:36 +0100

    Downgrade laravel
    Downgrade  php 8 to 7
    Downgrade laravel 9 to 8

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -18,10 +18,10 @@
 		<script defer="" src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"></script>
 		<script defer="" src="https://use.fontawesome.com/releases/v5.15.1/js/v4-shims.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-		<script src="runtime.6d637ef9e0c74dbe.js" type="module"></script>
+		<script src="runtime.f85e04a8661d7915.js" type="module"></script>
 		<script src="polyfills.7e16bdbffcf7884a.js" type="module"></script>
 		<script src="scripts.1085c94e712470f3.js" defer=""></script>
-		<script src="main.087b62fa2b0bd760.js" type="module"></script>
+		<script src="main.e35966cb5f6b6267.js" type="module"></script>
 		{{--angular scripts end--}}
     </body>
 </html>
