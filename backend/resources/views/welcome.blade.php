e593f69c2804bb06dd1c425c0f69e12145172741
Rumike7 <martintossou2@gmail.com>
2023-07-04 13:56:00 +0100

    first deploy

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -18,10 +18,10 @@
 		<script defer="" src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"></script>
 		<script defer="" src="https://use.fontawesome.com/releases/v5.15.1/js/v4-shims.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-		<script src="runtime.26f91ceda3d7e71d.js" type="module"></script>
+		<script src="runtime.6d637ef9e0c74dbe.js" type="module"></script>
 		<script src="polyfills.7e16bdbffcf7884a.js" type="module"></script>
 		<script src="scripts.1085c94e712470f3.js" defer=""></script>
-		<script src="main.874706ef282fb238.js" type="module"></script>
+		<script src="main.087b62fa2b0bd760.js" type="module"></script>
 		{{--angular scripts end--}}
     </body>
 </html>
